03e5e896296df2c9110bdb698c5202cb1ae1c24f
artem <artem.avvakumov@tuta.io>
2023-12-25 19:20:53 +0400

    Cleaning up

--- Day_01/main.py
+++ Day_01/main.py
@@ -1,15 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    test = 4
-    test += 50
-    print(f'Hi, {name} {test}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 def read_data() -> list:
     data = []
@@ -29,6 +17,7 @@ def get_calibration_value(string):
     digit_found = False
 
     digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
     for char in string:
         if char in digits:
             if digit_found:
@@ -54,5 +43,3 @@ if __name__ == '__main__':
 
     print("Total:")
     print(sum_of_values)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
